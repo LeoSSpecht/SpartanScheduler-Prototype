f32b89a2dd5b224dd226c9177559e471fa1387f5
Leo <leo.s.specht@gmail.com>
2022-04-09 02:23:08 -0400

    updates on server

--- server/api.js
+++ server/api.js
@@ -2,11 +2,17 @@
 
 const express = require("express")
 const app = express()
+const cors = require("cors")
+app.use(cors())
 
 app.get("/", function(req, res) {
   res.send("It's working!")
 })
 
-app.listen(3000, () => {
+app.get("/name", function(req, res) {
+  res.send({"name": "Spartan Hack"}) // Should be json format
+})
+
+app.listen(3001, () => {
   console.log("app listening on port 3000")
 })
