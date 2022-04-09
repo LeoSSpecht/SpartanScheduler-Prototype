792bc82aa704f013614cef0332288fc5ce37e26c
joelnataren9 <joelnataren9@hotmail.com>
2022-04-09 14:17:51 -0400

    Working on table

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import MainPage from "./pages/MainPage"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<MainPage />}/>
+        <Route exact path='/login' element={<LoginPage />}/>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function LoginPage() {
   return (
-    <div className="login-page row">
+    <div className="login-page row justify-content-center">
       <div class="input-field">
         <input id="emailSesion" type="email" required/>
         <label for="emailSesion">E-mail:</label>
